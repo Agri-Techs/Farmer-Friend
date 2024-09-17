86e5a5cdc9af100270d5426b51c95821fb1f402b
Agri-Techs <23k91a6740@tkrcet.com>
2024-09-17 20:28:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+// Script to toggle the registration form visibility
+document.getElementById('registerBtn').addEventListener('click', function() {
+  var form = document.getElementById('registrationForm');
+  if (form.style.display === "none") {
+      form.style.display = "block";
+  } else {
+      form.style.display = "none";
+  }
+});
+=======
+// Script to toggle the registration form visibility
+document.getElementById('registerBtn').addEventListener('click', function() {
+  var form = document.getElementById('registrationForm');
+  if (form.style.display === "none") {
+      form.style.display = "block";
+  } else {
+      form.style.display = "none";
+  }
+});
+>>>>>>> e889a75f6612eaa648d2fa846eccacf909d6fa09
